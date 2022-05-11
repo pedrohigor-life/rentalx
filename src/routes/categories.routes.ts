78ae9d1f441ad208a023c5b889064a5b4ad6e9df
pedrohigor-sousa <higorwork.sousa@gmail.com>
2022-05-11 19:45:26 -0300

    Adding models in application. Added model Category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,15 +1,21 @@
 import { Router } from 'express';
 
-import { v4 as uuidv4 } from 'uuid';
+import { Category } from '../models/Category';
 
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  const category = { id: uuidv4(), name, description };
+  const category: Category = new Category();
+
+  Object.assign(category, {
+    name,
+    description,
+    created_at: new Date(),
+  });
 
   categories.push(category);
 
