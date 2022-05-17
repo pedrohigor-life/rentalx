1ededb5314d0c61a5ae4163fb7f7fe6412d8e6ae
pedrohigor-sousa <higorwork.sousa@gmail.com>
2022-05-17 05:08:06 -0300

    Modify DTO

--- src/repositories/ICategoriesRepository.ts
+++ src/repositories/ICategoriesRepository.ts
@@ -1,13 +1,9 @@
+import { ICreateCategoryDTO } from '../dtos/ICreateCategoryDTO';
 import { Category } from '../models/Category';
-
-interface IRequest {
-  name: string;
-  description: string;
-}
 
 interface ICategoriesRepository {
   findbyName(name: string): Category;
-  create({ name, description }: IRequest): void;
+  create({ name, description }: ICreateCategoryDTO): void;
   index(): Category[];
 }
 
