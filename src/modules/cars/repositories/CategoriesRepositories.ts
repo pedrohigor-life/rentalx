d37878e6e5f22043693596d6fa321479364204a0
pedrohigor-sousa <higorwork.sousa@gmail.com>
2022-06-10 02:00:11 -0300

    Applying singleton desigtn pattern

--- src/modules/cars/repositories/CategoriesRepositories.ts
+++ src/modules/cars/repositories/CategoriesRepositories.ts
@@ -5,8 +5,18 @@ import { ICategoriesRepository } from './ICategoriesRepository';
 class CategoriesRepository implements ICategoriesRepository {
   private categories: Category[];
 
-  constructor() {
+  private static INSTANCE: CategoriesRepository;
+
+  private constructor() {
     this.categories = [];
+  }
+
+  public static getInstance(): CategoriesRepository {
+    if (!CategoriesRepository.INSTANCE) {
+      CategoriesRepository.INSTANCE = new CategoriesRepository();
+    }
+
+    return CategoriesRepository.INSTANCE;
   }
 
   public create({ name, description }: ICreateCategoryDTO): void {
--- src/modules/cars/useCases/indexCategory/index.ts
+++ src/modules/cars/useCases/indexCategory/index.ts
@@ -2,7 +2,7 @@ import { CategoriesRepository } from '../../repositories/CategoriesRepositories'
 import { IndexCategoryController } from './IndexCategoryController';
 import { IndexCategoryUseCase } from './IndexCategoryUseCase';
 
-const categoryRepository = new CategoriesRepository();
+const categoryRepository = CategoriesRepository.getInstance();
 const indexCategoryUseCase = new IndexCategoryUseCase(categoryRepository);
 
 const indexCategoryController = new IndexCategoryController(
