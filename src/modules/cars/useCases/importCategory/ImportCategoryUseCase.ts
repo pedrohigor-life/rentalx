060f4bf915e96ff1350b2abc15da9afa35ced1c0
pedrohigor-sousa <higorwork.sousa@gmail.com>
2022-06-29 01:08:57 -0300

    Implementing Promise in ready file

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,16 +1,45 @@
 import fs from 'fs';
 import { parse } from 'csv-parse';
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
+
+interface ImportCategory {
+  name: string;
+  description: string;
+}
+
 class ImportCategoryUseCase {
-  execute(file: Express.Multer.File): void {
-    const stream = fs.createReadStream(file.path);
+  constructor(private categoryRepository: ICategoriesRepository) {}
 
-    const parseFile = parse();
+  loadCategory(file: Express.Multer.File): Promise<ImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path);
+      const categories: ImportCategory[] = [];
 
-    stream.pipe(parseFile);
+      const parseFile = parse();
 
-    parseFile.on('data', async (line) => {
-      console.log(line);
+      stream.pipe(parseFile);
+
+      parseFile
+        .on('data', async (line) => {
+          const [name, description] = line;
+
+          categories.push({
+            name,
+            description,
+          });
+        })
+        .on('end', () => {
+          resolve(categories);
+        })
+        .on('error', (err) => {
+          reject(err);
+        });
     });
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategory(file);
+    console.log(categories);
   }
 }
 
