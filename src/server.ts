e1c95eda6598f3bcd9971c4e1b90c04d7ef75727
pedrohigor-sousa <higorwork.sousa@gmail.com>
2022-06-29 01:24:22 -0300

    installed and configured swagger

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 import { router } from './routes';
 
 const app = express();
 
+import swaggerFile from './swagger.json';
+
 app.use(express.json());
+
+app.use('/api=docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 app.use(router);
 
 app.listen(3333, () => {
